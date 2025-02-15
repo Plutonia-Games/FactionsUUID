ef3dad393385f56256f681b140d4395874310a38
Pierre-Olivier GOIN <pierreoliviergoin@gmail.com>
2025-02-15 22:51:20 +0100

    Remove brigadier support (it's 1.13+ and this is 1.8.8 support)

--- src/main/java/com/massivecraft/factions/cmd/FCmdRoot.java
+++ src/main/java/com/massivecraft/factions/cmd/FCmdRoot.java
@@ -23,7 +23,6 @@ import com.massivecraft.factions.landraidcontrol.DTRControl;
 import com.massivecraft.factions.landraidcontrol.PowerControl;
 import com.massivecraft.factions.util.TL;
 import io.papermc.lib.PaperLib;
-import me.lucko.commodore.CommodoreProvider;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -42,8 +41,6 @@ public class FCmdRoot extends FCommand implements CommandExecutor {
     }
 
     public final CmdAutoHelp cmdAutoHelp = new CmdAutoHelp();
-
-    public BrigadierManager brigadierManager;
 
     public final CmdAdmin cmdAdmin = new CmdAdmin();
     public final CmdAutoClaim cmdAutoClaim = new CmdAutoClaim();
@@ -134,10 +131,6 @@ public class FCmdRoot extends FCommand implements CommandExecutor {
         super();
 
         cmdBase = this;
-
-        if (canCommodore()) {
-            brigadierManager = new BrigadierManager();
-        }
 
         this.aliases.addAll(FactionsPlugin.getInstance().conf().getCommandBase());
         this.aliases.removeAll(Collections.<String>singletonList(null));  // remove any nulls from extra commas
@@ -255,9 +248,6 @@ public class FCmdRoot extends FCommand implements CommandExecutor {
             this.addSubCommand(new CmdSetMaxVaults());
             this.addSubCommand(new CmdVault());
         }
-        if (canCommodore()) {
-            brigadierManager.build();
-        }
     }
 
     @Override
@@ -280,17 +270,10 @@ public class FCmdRoot extends FCommand implements CommandExecutor {
     @Override
     public void addSubCommand(FCommand subCommand) {
         super.addSubCommand(subCommand);
-        if (canCommodore()) {
-            brigadierManager.addSubCommand(subCommand);
-        }
     }
 
     @Override
     public TL getUsageTranslation() {
         return TL.GENERIC_PLACEHOLDER;
-    }
-
-    private boolean canCommodore() {
-        return (FactionsPlugin.getMCVersion() < 1900 || PaperLib.isPaper()) && CommodoreProvider.isSupported();
     }
 }
